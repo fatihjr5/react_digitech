f259e68117080a6250650d48732a2f4793a0bddf
Atshilla Fatih Ibrahim <shilla.ibra@gmail.com>
2023-03-21 18:05:50 +0700

    update 21 maret

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -3,6 +3,7 @@ import Layout from '../../components/layout'
 import { CardMini } from '../../components/cards'
 import { commerce } from '../../lib/commerce'
 import { AiOutlineInbox, AiOutlineLoading3Quarters } from 'react-icons/ai';
+import { Input } from 'antd';
 
 function Index() {
   const [product, setProduct] = useState([]);
@@ -64,7 +65,9 @@ function Index() {
     <Layout handleGetCart={handleGetCart} cart={cart}>
         <section className="flex flex-col mx-auto gap-y-10">
             <h5 className="text-3xl font-medium text-center">Your One-Stop Shop for <br /> the Hottest Products</h5>
-            <input type="text" placeholder='Search Here' className='mx-auto border-b w-96 pl-4 pb-1 focus:outline-none' onChange={(e) => setSearchText(e.target.value)} />
+            <div className="flex flex-row items-center">
+              <Input className='w-96 py-2 mx-auto' placeholder="Search product" onChange={(e) => setSearchText(e.target.value)} />
+            </div>
         </section>
         { loading ? (
           <AiOutlineLoading3Quarters className='animate-spin duration-75 mx-auto mt-20'/>
