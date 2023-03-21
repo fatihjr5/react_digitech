01b9750b0bf880516cdf1bdf8c921277be85ff35
Atshilla Fatih Ibrahim <shilla.ibra@gmail.com>
2023-03-21 15:59:27 +0700

    update auto search by alphabet

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -6,6 +6,7 @@ import { commerce } from '../../lib/commerce'
 function Index() {
   const [product, setProduct] = useState([]);
   const [cart, setCart] = useState({});
+  const [searchText, setSearchText] = useState('');
 
   // get Product
   const getProduct = async() => {
@@ -20,6 +21,10 @@ function Index() {
     const item = await commerce.cart.add(productId, quantity);
     setCart(item);
   }
+  // search product 
+  const filteredProduct = product.filter((item) => {
+    return item.name.toLowerCase().includes(searchText.toLowerCase());
+  });
   // render
   useEffect(()=>{
     getProduct();
@@ -29,11 +34,11 @@ function Index() {
     <Layout handleGetCart={handleGetCart} cart={cart}>
         <section className="flex flex-col mx-auto gap-y-10">
             <h5 className="text-3xl font-medium text-center">Your One-Stop Shop for <br /> the Hottest Products</h5>
-            <input type="text" placeholder='Search Here' className='mx-auto border-b w-96 pl-4 pb-1 focus:outline-none' />
+            <input type="text" placeholder='Search Here' className='mx-auto border-b w-96 pl-4 pb-1 focus:outline-none' onChange={(e) => setSearchText(e.target.value)} />
         </section>
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-8 mt-20">
           {
-            product.map((item) => 
+            filteredProduct.map((item) => 
               <CardMini 
                 key={item.id} 
                 title={item.name} 
